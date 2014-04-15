f86cf390b827dfec9034e43b27d36d1144158859
android-andi <skate4life@gmx.de>
2014-04-16 01:00:40 +0200

    Upstream 3.0.43 -> 3.0.44
    https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/log/?h=linux-3.0.y
    
    Change-Id: I75320b96eabdb03826dac6924ccc83ae05e1ba98

--- drivers/ata/ahci.c
+++ drivers/ata/ahci.c
@@ -393,7 +393,9 @@ static const struct pci_device_id ahci_pci_tbl[] = {
 	{ PCI_DEVICE(0x1b4b, 0x9125),
 	  .driver_data = board_ahci_yes_fbs },			/* 88se9125 */
 	{ PCI_DEVICE(0x1b4b, 0x917a),
-	 .driver_data = board_ahci_yes_fbs },			/* 88se9172 */
+	  .driver_data = board_ahci_yes_fbs },			/* 88se9172 */
+	{ PCI_DEVICE(0x1b4b, 0x9192),
+	  .driver_data = board_ahci_yes_fbs },			/* 88se9172 on some Gigabyte */
 	{ PCI_DEVICE(0x1b4b, 0x91a3),
 	  .driver_data = board_ahci_yes_fbs },
 
